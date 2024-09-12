5def5b3d648456ac504b77d5d4869b0977d66ffd
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-13 00:21:51 +0530

    rotate arr by k

---
+++ easy/Rotate_K.java
@@ -0,0 +1,30 @@
+package easy;
+
+public class Rotate_K {
+    public void reverse(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotateK(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        Rotate_K r = new Rotate_K();
+        r.rotateK(nums, 3);
+        for (int i : nums) {
+            System.out.print(i);
+        }
+    }
+}
